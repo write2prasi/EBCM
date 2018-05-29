f2e6ca4d07b79f2995428b11c079d66ae58d85b4
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2018-05-29 22:25:10 +0530

    Revert "Added dependencies and data."

--- packrat/lib/x86_64-pc-linux-gnu/3.4.4/config/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(config)
-
-test_check("config")
--- packrat/lib/x86_64-pc-linux-gnu/3.4.4/config/tests/testthat/test-active.R
+++
@@ -1,4 +0,0 @@
-
-test_that("is_active can be used to test the active configuration", {
-  expect_true(config::is_active("default"))
-})
